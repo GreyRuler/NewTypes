5f59f3779666edff08c156f9cfdbaabaee53d0fc
GreyRuler <daniil_vasilenko757@mail.ru>
2022-10-08 15:40:58 +0300

    Impl

---
+++ src/ts/Movie.ts
@@ -0,0 +1,10 @@
+export default class Movie {
+    constructor(
+        readonly _title: string,
+        readonly _age: number,
+        readonly _country: string,
+        readonly _slogan: string,
+        readonly _genre: string,
+        readonly _time: string
+    ) { }
+}
